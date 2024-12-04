2ae42775bbaca24df3cd436f949cfdc3b68afc1d
Saveliy250 <savepo@icloud.com>
2024-12-04 20:40:56 +0300

    )

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   base: '/my-dApp',
+  build: {
+    outDir: 'dist',
+  },
 });
