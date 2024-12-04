99bbd51f8ae6ed6f688e486e3b1232e7994012f9
Saveliy250 <savepo@icloud.com>
2024-12-04 19:51:58 +0300

    finished working on website

--- src/hooks/useCounterContract.ts
+++ src/hooks/useCounterContract.ts
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react';
 import Counter from '../contracts/counter';
 import { useTonClient } from './useTonClient';
 import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
 import { Address, OpenedContract } from '@ton/core';
 
 export function useCounterContract() {
     const client = useTonClient();
     const [val, setVal] = useState<null | number>();
+    const { sender } = useTonConnect();
+
+    const sleep  = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
     const counterContract = useAsyncInitialize(async () => {
         if (!client) return;
@@ -22,6 +26,8 @@ export function useCounterContract() {
             setVal(null);
             const val = await counterContract.getCounter();
             setVal(Number(val));
+            await sleep(5000);
+            getValue();
         }
         getValue();
     }, [counterContract]);
@@ -29,5 +35,8 @@ export function useCounterContract() {
     return {
         value: val,
         address: counterContract?.address.toString(),
+        sendIncrement: () => {
+            return counterContract?.sendIncrement(sender);
+        },
     };
 }
